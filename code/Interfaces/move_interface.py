edf87ca99d17ef434e5c11209952a524a5f8ba2c
Paulo Victor <victorfduarte18@gmail.com>
2022-03-26 13:39:57 -0300

    atualização

--- code/Interfaces/move_interface.py
+++ code/Interfaces/move_interface.py
@@ -5,8 +5,15 @@ class MoveInterface:
         '''
         pass
 
+    def set_position(self, pos: 'tuple[int, int]'):
+        '''*Abstract Function*\n
+        Seta a posição X, Y do objeto
+        '''
+        pass
+
     def move(self, pos: 'tuple[int, int]') -> None:
         '''*Abstract Function*\n
         Move o objeto para a posição especificada. Pode implementar verificações que
-        permitam ou não a movimentação.'''
+        permitam ou não a movimentação.
+        '''
         pass
